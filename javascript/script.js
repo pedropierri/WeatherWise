820f2c4ab1faab28706cdefd07d6a8fca6fef27b
Pedro Pierri <pedropmp05@gmail.com>
2025-09-12 15:01:26 -0300

    basics

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+const cityName = 'london'
+const API_KEY = '21ef802a34c403fb671820438d5f4248'
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+
+console.log(API_URL)
